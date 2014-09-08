33f020e9bf229e9878aa9d247665377a4d898793
Tim Boode <tim@compuboot.in>
2014-09-08 16:28:00 +0200

    Homework done(2.11, 2.22, 2.28) + P2.29 + started on a browser gui for my homework

---
+++ PEScience2.29/src/pescience2/pkg29/PEScience229.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pescience2.pkg29;
+
+/**
+ *
+ * @author tim
+ */
+public class PEScience229 {
+    
+    static double drewPointTemperature;//Td in the formula, the end result.
+    static double b = 237.7;
+    static double a = 27.7;
+    //Td = (b * f(T,RH)) / (a - f(T,RH)) // this is the formula used to calculate the final drewPointTemperature
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ homeworkBrowserGUI/src/homeworkbrowsergui/HomeworkBrowserGUI.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package homeworkbrowsergui;
+
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+ 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+/**
+ *
+ * @author tim
+ */
+public class HomeworkBrowserGUI {//extends JPanel
+                        //implements ActionListener{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
